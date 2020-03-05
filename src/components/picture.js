9aba59d93b8139362b145b0285d20e404730c3bf
unknown <Kat2bk@gmail.com>
2020-03-05 15:16:09 -0800

    added a button component, will edit further

--- src/components/picture.js
+++ src/components/picture.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Buttons from "./buttons";
 
 export default function Picture(props) {
   return (
@@ -6,6 +7,8 @@ export default function Picture(props) {
       <h1>{props.postdata.title}</h1>
       <h2>{props.postdata.date}</h2>
       <img style={{ width: 500 }} alt="" src={props.postdata.hdurl} />
+      <Buttons />
+      <h3>{props.postdata.copyright}</h3>
     </div>
   );
 }
